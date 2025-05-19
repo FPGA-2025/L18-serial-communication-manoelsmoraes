bd0552c13fa8465f09622408be975538a5b58c0c
manoelsmoraes <63382033+manoelsmoraes@users.noreply.github.com>
2025-05-19 16:11:22 -0300

    erro em ciclos

--- receiver.v
+++ receiver.v
@@ -1,12 +1,49 @@
 module receiver (
     input clk,
     input rstn,
-    output ready,
-    output [6:0] data_out,
-    output parity_ok_n,
+    output reg ready,
+    output reg [6:0] data_out,
+    output reg parity_ok_n,
     input serial_in
 );
 
-//insira seu código aqui
+    reg [3:0] bit_cnt;
+    reg [7:0] shift_reg;
+    reg receiving;
+
+    always @(posedge clk or negedge rstn) begin
+        if (!rstn) begin
+            receiving <= 0;
+            bit_cnt <= 0;
+            shift_reg <= 0;
+            data_out <= 0;
+            ready <= 0;
+            parity_ok_n <= 1; // isso precisa estar em 1 para indicar que não há dados válidos
+        end else begin
+            ready <= 0;
+
+            if (!receiving) begin
+                if (serial_in == 0) begin // detecta start bit
+                    receiving <= 1;
+                    bit_cnt <= 0;
+                end
+            end else begin
+                 bit_cnt <= bit_cnt + 1;
+
+                if (bit_cnt < 8) begin
+                    // Armazena 7 bits de dados + paridade (bit_cnt 0 a 7)
+                    shift_reg <= {serial_in, shift_reg[7:1]};
+                end
+
+                if (bit_cnt == 8) begin
+                    // Após o 8º bit recebido (bit de paridade)
+                    receiving <= 0;
+                    data_out <= shift_reg[6:0];
+                    parity_ok_n <= ^{shift_reg[6:0], serial_in}; // paridade dos dados + último bit
+                    ready <= 1;
+                end
+            end
+        end
+    end
 
 endmodule
--- transmitter.v
+++ transmitter.v
@@ -6,6 +6,37 @@ module transmitter (
     output reg serial_out
 );
 
-//insira seu código aqui
+    reg [3:0] bit_cnt;
+    reg [7:0] shift_reg;
+    reg sending;
+
+    wire parity_bit = ~(^data_in); // paridade par
+
+    always @(posedge clk or negedge rstn) begin
+        if (!rstn) begin
+            serial_out <= 1'b1; // linha em repouso
+            sending <= 0;
+            bit_cnt <= 0;
+            shift_reg <= 0;
+        end else begin
+            if (start && !sending) begin
+                sending <= 1;
+                bit_cnt <= 0;
+                shift_reg <= {parity_bit, data_in}; // [paridade, D6...D0]
+                serial_out <= 1'b0; // start bit
+            end else if (sending) begin
+                bit_cnt <= bit_cnt + 1;
+                if (bit_cnt < 8)
+                    serial_out <= shift_reg[bit_cnt];
+                else begin
+                    serial_out <= 1'b1; // linha volta ao repouso
+                    sending <= 0;
+                end
+            end else begin
+                serial_out <= 1'b1; // linha em repouso
+                // Não faz nada se não estiver enviando
+            end
+        end
+    end
 
 endmodule
